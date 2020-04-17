56720df7f86622d35aa29c4cf8ab3e369292e5ed
Macbook <macbook@MacbookdeMacBook-Pro.local>
2020-04-17 15:55:08 +0800

    新建Springboot工程

---
+++ src/test/java/com/spdb/boot001/Boot001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spdb.boot001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
